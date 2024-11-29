debff2c63ad5d4bc699e064d32d6b1309c4dc0d4
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-11-29 14:50:21 +0300

    configure vector store data load

--- src/main/java/com/captain/spring_ai_rag/config/VectorStoreConfig.java
+++ src/main/java/com/captain/spring_ai_rag/config/VectorStoreConfig.java
@@ -1,22 +1,45 @@
 package com.captain.spring_ai_rag.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Created by Jackson G. Mwaniki
  */
+@Slf4j
 @Configuration
 public class VectorStoreConfig {
 
     @Bean
     public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel, VectorStoreProperties vectorStoreProperties) {
-        SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingModel);
+        var store = new SimpleVectorStore(embeddingModel);
         File vectorStoreFile = new File(vectorStoreProperties.getVectorStorePath());
-        return simpleVectorStore;
+
+        if (!vectorStoreFile.exists()) {
+            store.load(vectorStoreFile);
+        } else {
+            System.out.println("Loading documents into vector store");
+            vectorStoreProperties.getDocumentsToLoad().forEach(document -> {
+                log.debug("Loading document: {}", document.getFilename());
+                TikaDocumentReader documentReader = new TikaDocumentReader(document);
+                List<Document> docs = documentReader.get();
+                TextSplitter textSplitter = new TokenTextSplitter();
+                List<Document> splitDocs = textSplitter.apply(docs);
+                store.add(splitDocs);
+
+            });
+            store.save(vectorStoreFile);
+        }
+        return store;
     }
 }
